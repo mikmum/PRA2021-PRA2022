6d53414316b901ab2ecdb45f7a37ad4ec55889cd
mikmum <cobblechups@gmail.com>
2021-10-04 16:45:02 +0200

    cw1

--- src/main/java/introduction/HelloWorld.java
+++ src/main/java/introduction/HelloWorld.java
@@ -1,8 +1,12 @@
 package introduction;
 
+import java.util.logging.Logger;
+
 public class HelloWorld {
 
+    final static Logger log = Logger.getLogger("name");
     public static void main(String [ ] args) {
         System.out.println("Hello! Witaj!");
+        log.info("dzien dobry");
     }
 }
